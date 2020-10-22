d7d6400a1a5b007917c2dce6837251f957750bf4
Kilovolt <lumiere860@gmail.com>
2020-10-22 19:46:23 -0400

    adding predict.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from fastapi import FastAPI, Request, Form, Query
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 #from app.api import estimate  # , viz
+from app.api import predict 
+
+
+
 
 #INIT
 app = FastAPI(
@@ -14,7 +18,7 @@ app = FastAPI(
 
 #app.include_router(estimate.router)
 # app.include_router(viz.router)
-
+app.include_router(predict.router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,46 +29,15 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+
+
 templates = Jinja2Templates(directory="templates/")
 
 #Description of the data we will be loading into model:
-class Song:
-    def __init__(self, artist, name, year, genre, duration):
-        self.artist = artist
-        self.name = name
-        self.year = year 
-        self.genre = genre
-        self.duration = duration
 
 
-        
-
-
-#Creating some songs just to test how it works:
-
-Rock1 = Song("Generic_band", "Crapcore", 1969, "metalcore", 3.5)
-Rock2 = Song("Generic_band", "No_muffler", 2004, "heavy metal", 4)
-
-artist_list = ['Generic_band', 'Generic_artist', 'Generic_singer']
-song_list = ["Heavy Metal1", 'Glam Rock', "Jazz", "Drumnbass", "Rap", "Hell sounds"]
-
-
-
-
-
-
-
-
-@app.get("/get_data")
-async def get_data(_q: str = Query("Jazz", enum=song_list)):
-    # _q creates a dropdown menu with a selected options. 
-    #The Default option is "Jazz". The list of available options is enum=song_list
-    
-     #Prediction goes in here
-    
-    
-    return {"Prediction, based on input": _q}
-
+#removing empty values from 'genre' column
+#genres = genres[genres['genre'] != '']
 
 
     
---
+++ api/predict.py
@@ -0,0 +1,125 @@
+import pandas as pd
+from fastapi import FastAPI, Request, Form, Query, APIRouter
+import logging
+import pandas as pd
+import numpy as np
+from surprise import Reader, Dataset, SVD 
+from surprise.model_selection.validation import cross_validate
+from pydantic import BaseModel, Field
+
+
+log = logging.getLogger(__name__)
+router = APIRouter()
+
+class User(BaseModel):
+    user: int=Field(..., example=155)
+    
+@router.post('/predict')
+
+
+def load_data():
+    DATA_PATH_GENRE = 'https://raw.githubusercontent.com/Spotify-Song-Predict-DS18/Song-Predict/main/data/data_w_genres.csv'
+    DATA_PATH_GENRE_2 = 'https://raw.githubusercontent.com/Spotify-Song-Predict-DS18/Song-Predict/main/data/data_by_genres.csv'
+    DATA_PATH_ARTIST = 'https://raw.githubusercontent.com/Spotify-Song-Predict-DS18/Song-Predict/main/data/data_by_artist.csv'
+    DATA_PATH = 'https://raw.githubusercontent.com/Spotify-Song-Predict-DS18/Song-Predict/main/data/data.csv'
+    genres = pd.read_csv(DATA_PATH_GENRE)
+    genre2 = pd.read_csv(DATA_PATH_GENRE_2)
+    artist = pd.read_csv(DATA_PATH_ARTIST)
+    control = pd.read_csv(DATA_PATH)
+
+
+
+def clean_data():
+    list_of_genres = []
+    for i in genres['genres']:
+        i = i[1:-1].replace("'", "")
+        list_of_genres.append(i)
+
+    genres.drop(['genres'], axis=1, inplace=True)
+    genres = genres.assign(New_genres=list_of_genres)
+    genres = genres.rename(columns={'New_genres':'genre', 'artists':'artist'})
+
+    list_of_artists  = []
+    for k in control['artists']:
+        k = k[1:-1].replace("'", "")
+        list_of_artists.append(k)
+    list_of_artists = []   
+    control.drop(['artists'], axis=1, inplace=True)
+    control = control.assign(New_artists=list_of_artists)
+    control = control.rename(columns={'New_artists':'artist'})
+
+
+def preprocessing():
+    result = pd.merge(control, genres[['artist', 'genre']], how='left', on='artist')
+    result = result[result['genre'].isnull() == False]
+    result['release_date'] = pd.to_datetime(control['release_date'])
+    result['user'] = np.random.randint(0, 200, result.shape[0])
+
+def model_training():
+    reader = Reader(rating_scale=(0,100))
+    data = Dataset.load_from_df(result[['user', 'id', 'popularity']],
+                           reader)
+
+    svd = SVD()
+    cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5,
+              verbose=True)
+    trainset = data.build_full_trainset()
+    svd.fit(trainset)
+
+#Running functions
+load_data()
+clean_data()
+preprocessing()
+model_training()
+
+
+     
+def recommend(userid, df):
+    '''A function that will take a userid and predict music based their listen history
+    With this database, will only take ids ranging 0-200.
+    
+    Returns a new column called "Estimate Score" which represents the chance that the
+    use will like the music
+    
+    Score will not be high on average, since users are randomly generated.'''
+    
+    # First passing through a if statment to make sure 
+    # The ID matches the requirements. 
+    if userid > 200:
+        return "ERROR: User ID too high, must be between 0-200"
+    elif userid < 0:
+        return "ERROR: User ID too low, must be between 0-200"
+    else:
+        print("Generating music recommendation...")
+    
+    # Intantiating my reader and my data
+    reader = Reader(rating_scale=(0,100))
+    data = Dataset.load_from_df(result[['user', 'id', 'popularity']],
+                            reader)
+    # Intatntiating my SVD 
+    svd = SVD()
+    
+    # Running a 5-fold cross-validation 
+    cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5,
+                    verbose=True)
+    
+    # Retraining the model using the entire dataset
+    trainset = data.build_full_trainset()
+    svd.fit(trainset)
+    
+    # Model has been trained, time to use for prediction.
+    titles = result.copy()
+
+    titles['Estimate_Score'] = titles['id'].apply(lambda x: svd.predict(userid, x).est)
+    
+    # Creating a mask that does not include songs the user already has. 
+    mask = titles['user'] != userid
+    titles = titles[mask]
+    
+    # Now returning the top 5 songs for that user.
+    titles = titles.sort_values(by=['Estimate_Score'], ascending=False)
+    
+    titles = titles.head(5)
+    
+    titles = [titles.columns.values.tolist()] + titles.values.tolist()
+    return titles
